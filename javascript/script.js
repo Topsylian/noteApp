48547adc26e8f612eba6e481262eae4c6a30edeb
Topsylian <topsylian@gmail.com>
2023-05-13 19:27:38 +0300

    a

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
 const body = document.querySelector("body");
 const addBtn = document.querySelector("button");
+const inputText = document.querySelector("input[type=text]");
 const replyEmoji = document.querySelector("section.reply-img > img");
 
-addBtn.addEventListener("click", () => {
-    replyEmoji."src" = "./icon/party.png";
+inputText.addEventListener("focus", () => {
+	replyEmoji.style.display = "block";
+	replyEmoji.src = './icon/thinking.png';
+	replyEmoji.classList.add('shake');
+	replyEmoji.classList.remove('forward');
 });
+
+inputText.addEventListener("blur", () => {
+	replyEmoji.src = './icon/party.png';
+	replyEmoji.classList.add('forward');
+	replyEmoji.classList.remove('shake');
+})
