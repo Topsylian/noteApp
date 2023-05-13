b421f48adaed9461b3332191d97359dc9c868494
Tomsree <88626625+Topsylian@users.noreply.github.com>
2023-05-13 16:59:09 +0300

    Update script.js
    changed

--- javascript/script.js
+++ javascript/script.js
@@ -3,5 +3,5 @@ const addBtn = document.querySelector("button");
 const replyEmoji = document.querySelector("section.reply-img > img");
 
 addBtn.addEventListener("click", () => {
-    replyEmoji.setAttribute("src", "./icon/delete.png");
+    replyEmoji.setAttribute("src", "./icon/party.png");
 });
